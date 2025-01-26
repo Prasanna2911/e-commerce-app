d825eb06c1e3d4dedbd912e557ee99d72fb29d1a
Prasanna2911 <bpp1757@gmail.com>
2025-01-27 00:37:52 +0530

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      {/* <h1 className="bg-red">Hello World</h1> */}
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/collection" element={<Collection></Collection>}></Route>
+        <Route path="/about" key={<About></About>}></Route>
+        <Route path="/contact" key={<Contact></Contact>}></Route>
+        <Route path="product/productId" element={<Product></Product>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="place-order" key={<PlaceOrder></PlaceOrder>}></Route>
+        <Route path="orders" key={<Orders></Orders>}></Route>
       </Routes>
     </div>
   );
