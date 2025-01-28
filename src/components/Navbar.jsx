bf215db65185dc5a78dae107019f536242a901e2
Prasanna2911 <bpp1757@gmail.com>
2025-01-29 01:00:33 +0530

    hero page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             alt="Profile-Icon"
             className="w-5 cursor-pointer"
           />
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+          <div className="group-hover:block hidden absolute  right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
               <p className="cursor-pointer hover:text-black">Orders</p>
